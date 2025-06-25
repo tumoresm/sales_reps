24ffc8b3e94db33e41116afcd4ce0b8ead295c83
GoldSol <tudesign.sa@gmail.com>
2025-06-25 03:28:27 +0200

    created app theme and colour scheme or pallete

---
+++ lib/core/theme/theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:sales_reps/core/theme/app_colours.dart';
+
+class AppTheme {
+  static final darkThemeMode = ThemeData.dark().copyWith(
+    scaffoldBackgroundColor: Colours.backgroundColor,
+  );
+}
