3fc945816cc0fd0d5589ca8588230d1d98ab67f2
GoldSol <tudesign.sa@gmail.com>
2025-06-25 20:54:33 +0200

    auth widgets designed and coded

---
+++ lib/features/auth/view/widgets/auth_button.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:sales_reps/core/theme/app_colours.dart';
+
+class AuthButton extends StatelessWidget {
+  const AuthButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: const LinearGradient(
+          colors: [
+            Colours.gradient1,
+            Colours.gradient2,
+          ],
+          begin: Alignment.bottomLeft,
+          end: Alignment.topRight,
+        ),
+        borderRadius: BorderRadius.circular(7),
+      ),
+      child: ElevatedButton(
+        onPressed: () {},
+        style: ElevatedButton.styleFrom(
+          fixedSize: const Size(395, 55),
+          backgroundColor: Colours.transparentColor,
+          shadowColor: Colours.transparentColor,
+        ),
+        child: Text(
+          'Sign Up',
+          style: TextStyle(
+            fontSize: 17,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/features/auth/view/widgets/custom_field.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class CustomField extends StatelessWidget {
+  final String hintText;
+  final TextEditingController controller;
+  final bool isObscureText;
+  const CustomField({
+    super.key,
+    required this.hintText,
+    required this.controller,
+    this.isObscureText = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      decoration: InputDecoration(
+        hintText: hintText,
+      ),
+      obscureText: isObscureText,
+    );
+  }
+}
