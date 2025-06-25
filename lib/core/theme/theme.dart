122b3ea2a26f46c13da21984333836f391d5d170
GoldSol <tudesign.sa@gmail.com>
2025-06-25 20:56:57 +0200

    signup page page design with the widgets in theme

--- lib/core/theme/theme.dart
+++ lib/core/theme/theme.dart
@@ -2,7 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:sales_reps/core/theme/app_colours.dart';
 
 class AppTheme {
+  
+  static OutlineInputBorder _border(Color color) => OutlineInputBorder(
+        borderSide: BorderSide(
+          color: color,
+          width: 3,
+        ),
+        borderRadius: BorderRadius.circular(10),
+      );
+
   static final darkThemeMode = ThemeData.dark().copyWith(
     scaffoldBackgroundColor: Colours.backgroundColor,
+    inputDecorationTheme: InputDecorationTheme(
+      contentPadding: const EdgeInsets.all(27),
+      enabledBorder: _border(Colours.borderColor),
+
+      focusedBorder: _border(Colours.gradient2),
+
+
+    ),
   );
 }
