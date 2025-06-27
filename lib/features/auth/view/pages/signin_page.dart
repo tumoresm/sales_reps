3cff69b46c27642ebc1ac1b3a56c8fefe1e4b69a
GoldSol <tudesign.sa@gmail.com>
2025-06-27 22:45:42 +0200

    Add sign-in page with email and password fields

---
+++ lib/features/auth/view/pages/signin_page.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:sales_reps/core/theme/app_colours.dart';
+import 'package:sales_reps/features/auth/view/widgets/auth_button.dart';
+import 'package:sales_reps/features/auth/view/widgets/custom_field.dart';
+
+class SigninPage extends StatefulWidget {
+  const SigninPage({super.key});
+
+  @override
+  State<SigninPage> createState() => _SignupPageState();
+}
+
+class _SignupPageState extends State<SigninPage> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15.0),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Sign In.',
+                style: TextStyle(
+                  fontSize: 50,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 30.0),
+              CustomField(
+                hintText: 'Email',
+                controller: emailController,
+              ),
+              const SizedBox(height: 15.0),
+              CustomField(
+                hintText: 'Password',
+                controller: passwordController,
+                isObscureText: true,
+              ),
+              const SizedBox(height: 20.0),
+              AuthButton(
+                buttonText: 'Sign In',
+                onTap: () {},
+              ),
+              const SizedBox(height: 20.0),
+              RichText(
+                text: TextSpan(
+                  text: 'Don\'t have an account? ',
+                  style: Theme.of(context).textTheme.titleMedium,
+                  children: const [
+                    TextSpan(
+                      text: 'Sign Up',
+                      style: TextStyle(
+                        color: Colours.gradient2,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
